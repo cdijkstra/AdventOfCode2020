b9e42821b0847e331c8101bd02668e9d332adbc0
Casper Dijkstra <cdijkstra@xpirit.com>
2020-12-01 17:42:59 +0100

    C++ solution for exercise day 1

---
+++ Day1/Day1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+void calculateProduct(std::vector<size_t> const& vec)
+{
+    for (size_t idx = 0; idx != vec.size(); ++idx){
+        for (size_t idy = idx; idy != vec.size(); ++idy) {
+            for (size_t idz = idy; idz != vec.size(); ++idz){
+                if (vec.at(idx) + vec.at(idy) + vec.at(idz) == 2020){
+                    std::cout << "The multiplied numbers are " << vec.at(idx) * vec.at(idy) * vec.at(idz) << '\n';
+                    return;
+                }
+            }
+        }
+    }
+    std::cout << "No numbers sum up to 2020\n";
+}
+
+int main()
+{
+    std::ifstream file;
+    file.open("data");
+    size_t expense;
+    std::vector<size_t> vec;
+    while (file >> expense)
+    {
+        vec.push_back(expense);
+    }
+    calculateProduct(vec);
+}
