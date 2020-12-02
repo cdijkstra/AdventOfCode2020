c494581c36f1b128c824ed12af97ea04c2f5a653
Casper Dijkstra <cdijkstra@xpirit.com>
2020-12-02 08:58:08 +0100

    Prevent using data in vector multiple times

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -4,8 +4,8 @@
 
 void calculateProduct(std::vector<size_t> const& vec)
 {
-    for (size_t idx = 0; idx != vec.size(); ++idx){
-        for (size_t idy = idx; idy != vec.size(); ++idy) {
+    for (size_t idx = 0; idx != vec.size() - 2; ++idx){
+        for (size_t idy = idx; idy != vec.size() - 1; ++idy) {
             for (size_t idz = idy; idz != vec.size(); ++idz){
                 if (vec.at(idx) + vec.at(idy) + vec.at(idz) == 2020){
                     std::cout << "The multiplied numbers are " << vec.at(idx) * vec.at(idy) * vec.at(idz) << '\n';
