3f776e8f54bf4634f643e3c9f0ff7ffe56a935e5
Casper Dijkstra <cdijkstra@xpirit.com>
2020-12-08 23:28:53 +0100

    Solution day 5

--- Day5/boardingpass.py
+++ Day5/boardingpass.py
@@ -2,22 +2,22 @@ import math
 
 bps = [line.rstrip() for line in open('data.txt')]
 
-def findRow(ch, index, min, max):
+def findRow(ch, index, range):
     while(index + 1 <= len(ch)):
         if (ch[index] == 'F'):
-            return findRow(ch, index + 1, min, math.floor((min + max) / 2))
+            return findRow(ch, index + 1, range(min, math.floor((min + max) / 2)))
         else:
-            return findRow(ch, index + 1, math.ceil((min + max) / 2), max)
+            return findRow(ch, index + 1, range(math.ceil((min + max) / 2), max))
     if (min != max):
         print("WARNING")
     return max
 
-def findColumn(ch, index, min, max):
+def findColumn(ch, index, range):
     while(index + 1 <= len(ch)):
         if (ch[index] == 'L'):
-            return findColumn(ch, index + 1, min, math.floor((min + max) / 2))
+            return findColumn(ch, index + 1, range(min, math.floor((min + max) / 2)))
         else:
-            return findColumn(ch, index + 1, math.ceil((min + max) / 2), max)
+            return findColumn(ch, index + 1, range(math.ceil((min + max) / 2), max))
     if (min != max):
         print("WARNING")
     return max
