656f8e5bbb5454a577f70f5cb9234556cb81298a
Casper Dijkstra <cdijkstra@xpirit.com>
2020-12-08 23:46:52 +0100

    Solution day 5

--- Day5/boardingpass.py
+++ Day5/boardingpass.py
@@ -2,31 +2,35 @@ import math
 
 bps = [line.rstrip() for line in open('data.txt')]
 
-def findRow(ch, index, range):
+def findRow(ch, index, min, max):
     while(index + 1 <= len(ch)):
         if (ch[index] == 'F'):
-            return findRow(ch, index + 1, range(min, math.floor((min + max) / 2)))
+            return findRow(ch, index + 1, min, math.floor((min + max) / 2))
         else:
-            return findRow(ch, index + 1, range(math.ceil((min + max) / 2), max))
+            return findRow(ch, index + 1, math.ceil((min + max) / 2), max)
     if (min != max):
         print("WARNING")
     return max
 
-def findColumn(ch, index, range):
+def findColumn(ch, index, min, max):
     while(index + 1 <= len(ch)):
         if (ch[index] == 'L'):
-            return findColumn(ch, index + 1, range(min, math.floor((min + max) / 2)))
+            return findColumn(ch, index + 1, min, math.floor((min + max) / 2))
         else:
-            return findColumn(ch, index + 1, range(math.ceil((min + max) / 2), max))
+            return findColumn(ch, index + 1, math.ceil((min + max) / 2), max)
     if (min != max):
         print("WARNING")
     return max
 
 def seatId(row, column):
-    print(row * 8 + column)
+    return row * 8 + column
 
-# for bp in bps:
-bp = "FBFBBFFRLR"
-row = findRow(bp[:7], 0, 0, 127)
-column = findColumn(bp[-3:], 0, 0, 7)
-id = seatId(row, column)
+maxfoundid = 0
+for bp in bps:
+    row = findRow(bp[:7], 0, 0, 127)
+    column = findColumn(bp[-3:], 0, 0, 7)
+    id = seatId(row, column)
+    if (id > maxfoundid):
+        maxfoundid = id
+
+print(maxfoundid)
